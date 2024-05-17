96cddf87c7a7c71e25955933db017c47b53a7125
abujafar01 <126306175+abujafar01@users.noreply.github.com>
2024-05-17 18:45:35 +0600

    resource/admin

--- home.blade.php
+++ home.blade.php
@@ -1,9 +1,22 @@
-<!DOCTYPE>
-<html>	
-<!-- 
+
+
+@include('admin.layout.header')
+
+<!--
 Style the page  as it appears in the example. -->
 <!-- DO NOT CHANGE ANY PART OF THIS HTML CODE!!! -->
-<body>
+
+<header>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<h1> Web Design is Awesome!!!</h1>
+	<nav>
+		<a href="http://www.umich.edu/">University of Michigan</a>
+		<a href="http://www.intro-webdesign.com/">Intro to Web Design</a>
+	</nav>
+  </header>
+
+
   <section id = "left">
 	<h2>Why I am taking this class</h2>
 	<ol>
@@ -31,6 +44,14 @@ Style the page  as it appears in the example. -->
 		<img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTzDojh5E0fzvTg4nWYs0JadpTwcS2S1KHOtnVQnlruNqmNvfIk" alt="Football">
 	</div>
   </section>
-  </body>
-</html>
- 
+
+
+  <footer>
+  	<p>Sample code for Responsive Design .<br/> Colleen van Lent</p>
+  </footer>
+<!-- DO NOT CHANGE ANY PART OF THIS HTML CODE!!! -->
+
+@include('admin.layout.footer')
+
+
+
