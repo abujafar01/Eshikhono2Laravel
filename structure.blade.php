33d5f53f8bef40cdbc2b216ef4509d4618797f93
abujafar01 <126306175+abujafar01@users.noreply.github.com>
2024-05-17 18:46:22 +0600

    resource/layout

--- structure.blade.php
+++ structure.blade.php
@@ -1,5 +1,8 @@
+
 <!DOCTYPE>
-<html>
+@extends('admin.layout.structure')
+@section('middle')
+
 <header>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,4 +12,17 @@
 		<a href="http://www.intro-webdesign.com/">Intro to Web Design</a>
 	</nav>
  </header>
- <html>
+ </html>
+
+
+
+
+@yield('middle')
+
+
+
+ <footer>
+  	<p>Sample code for Responsive Design .<br/> Colleen van Lent</p>
+  </footer>
+<!-- DO NOT CHANGE ANY PART OF THIS HTML CODE!!! -->
+@endsection
